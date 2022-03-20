b14ce7f74cf8f6feeb20466efb6ea49580b77161
Baiaman Kodirov <baiamankodirov@MacBook-Pro-Baiaman.local>
2022-03-20 16:30:40 +0600

    add new function, logic fix

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("data", () => ({
     articles: [],
     name: "articles",
-    viewCount: 10,
+    userId: 1,
     async init() {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((item) => item.json())
-        .then((item) => this.articles = item);
+      if (this.articles.length <= 0) {
+        this.userId = 1;
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?userId=${this.userId}`
+        )
+          .then((item) => item.json())
+          .then((item) => {
+            this.articles = item;
+          });
+      }
+    },
+    getNewArticles: async function () {
+      if (this.articles === []) {
+        this.init();
+      } else {
+        this.userId += 1;
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?userId=${this.userId}`
+        )
+          .then((item) => item.json())
+          .then((item) => {
+            this.articles.push(...item);
+          }).catch(err => {
+            alert("not new articles");
+          })
+      }
+      console.log(this.articles)
     },
   }));
 });
