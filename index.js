f8b313c42ad19c074523d937d09a35204bd2e72b
Baiaman Kodirov <baiamankodirov@MacBook-Pro-Baiaman.local>
2022-03-21 12:19:52 +0600

    fixed the bug

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 document.addEventListener("alpine:init", () => {
-  Alpine.data("data", () => ({
+  Alpine.data("Articles", () => ({
     articles: [],
     name: "articles",
     userId: 1,
-    async init() {
-      if (this.articles.length <= 0) {
-        this.userId = 1;
-        const res = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?userId=${this.userId}`
-        )
-          .then((item) => item.json())
-          .then((item) => {
-            this.articles = item;
-          });
-      }
-    },
-    getNewArticles: async function () {
-      if (this.articles === []) {
-        this.init();
-      } else {
-        this.userId += 1;
-        const res = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?userId=${this.userId}`
-        )
-          .then((item) => item.json())
-          .then((item) => {
-            this.articles.push(...item);
-          }).catch(err => {
-            alert("not new articles");
-          })
-      }
-      console.log(this.articles)
+    fethcArticles: async function () {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${this.userId}`
+      );
+      const data = await res.json();
+      this.articles.push(...data);
+      this.userId++;
     },
   }));
 });
